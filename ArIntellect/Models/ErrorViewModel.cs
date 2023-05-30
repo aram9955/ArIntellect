88340379b2478a6e2b6db65f1a9ac1340aa841ff
Aram Baghdasarian <aram99551@gmail.com>
2023-05-30 23:18:05 +0400

    Add project files.

---
+++ ArIntellect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArIntellect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
